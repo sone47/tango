bd503f0def47f4ebc4f4fdc3f2d29aea6b082e13
sone <sone47.china@gmail.com>
2025-08-27 21:13:46 +0800

    feat(PracticeHeader): add card pack name display and remove unnecessary null check for selected card pack

--- src/pages/game/components/PracticeHeader.tsx
+++ src/pages/game/components/PracticeHeader.tsx
@@ -3,19 +3,20 @@ import { useNavigate } from 'react-router'
 
 import AlertDialog from '@/components/AlertDialog'
 import Button from '@/components/Button'
+import Typography from '@/components/Typography'
 import { usePracticeStore } from '@/stores/practiceStore'
 
 const PracticeHeader = () => {
   const navigate = useNavigate()
   const { selectedCardPack, currentWordIndex, shuffledWords, updateState } = usePracticeStore()
-  if (!selectedCardPack || currentWordIndex >= shuffledWords.length) return null
+  if (currentWordIndex >= shuffledWords.length) return null
 
   const handleExitPractice = () => {
     navigate(-1)
   }
 
   return (
-    <div className="flex items-center justify-between p-2 bg-background backdrop-blur-sm">
+    <div className="flex items-center justify-between p-2 bg-background">
       <AlertDialog
         title="确定要退出本次学习吗？"
         description=" "
@@ -24,6 +25,8 @@ const PracticeHeader = () => {
         onConfirm={handleExitPractice}
         trigger={<Button variant="ghost" size="md" icon={ChevronLeft} className="" />}
       ></AlertDialog>
+
+      <Typography.Title level={6}>{selectedCardPack?.name}</Typography.Title>
 
       <Button
         variant="ghost"
