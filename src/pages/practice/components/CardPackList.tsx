c88be27072fff3d7392c7244c41e4daee24ba0f4
sone <sone47.china@gmail.com>
2025-09-02 14:54:13 +0800

    fix(PracticeTab): hasData component rendered when data not fetched

--- src/pages/practice/components/CardPackList.tsx
+++ src/pages/practice/components/CardPackList.tsx
@@ -1,41 +1,14 @@
-import { useEffect } from 'react'
-
-import EmptyWordPack from '@/components/EmptyWordPack'
-import Loading from '@/components/Loading'
-import { useCardPacks } from '@/hooks/useCardPacks'
-import { useCurrentWordPack } from '@/hooks/useCurrentWordPack'
 import { useLastestData } from '@/hooks/useLastestData'
-import { useWordPackStore } from '@/stores/wordPackStore'
+import { CardPack } from '@/types'
 
 import CardPackItem from './CardPackItem'
 
-const CardPackList = () => {
-  const { cardPacks, loading, fetchCardPacks } = useCardPacks()
-  const { hasData } = useWordPackStore()
-  const { currentWordPack } = useCurrentWordPack()
+interface CardPackListProps {
+  cardPacks: (CardPack & { progress: number })[]
+}
+
+const CardPackList = ({ cardPacks }: CardPackListProps) => {
   const { cardPackId: latestCardPackId } = useLastestData()
-
-  useEffect(() => {
-    if (currentWordPack) {
-      fetchCardPacks(currentWordPack.id)
-    }
-  }, [currentWordPack])
-
-  if (loading) {
-    return (
-      <div className="h-full">
-        <Loading text="加载卡包中..." size="md" />
-      </div>
-    )
-  }
-
-  if (!hasData) {
-    return (
-      <div className="h-full pb-4">
-        <EmptyWordPack showImportButton />
-      </div>
-    )
-  }
 
   return (
     <div className="flex flex-col gap-9 py-4">
--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -1,6 +1,10 @@
 import { isNil } from 'lodash'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
+import EmptyWordPack from '@/components/EmptyWordPack'
+import Loading from '@/components/Loading'
+import { useCardPacks } from '@/hooks/useCardPacks'
+import { useCurrentWordPack } from '@/hooks/useCurrentWordPack'
 import { useLastestData } from '@/hooks/useLastestData'
 import { usePracticeStore } from '@/stores/practiceStore'
 import { useWordPackStore } from '@/stores/wordPackStore'
@@ -9,24 +13,51 @@ import CardPackConfigModal from './components/CardPackConfigModal'
 import CardPackList from './components/CardPackList'
 
 export default function PracticeTab() {
-  const { fetchWordPacks } = useWordPackStore()
+  const { fetchWordPacks, hasData, loading: fetchWordPacksLoading } = useWordPackStore()
+  const { currentWordPackId } = useCurrentWordPack()
   const { resetPracticeState } = usePracticeStore()
   const { cardPackId: latestCardPackId } = useLastestData()
+  const { cardPacks, loading: fetchCardPacksLoading, fetchCardPacks } = useCardPacks()
+
+  const [hasInit, setHasInit] = useState(false)
 
   useEffect(() => {
     resetPracticeState()
 
-    fetchWordPacks()
+    fetchData()
   }, [])
+
+  const fetchData = async () => {
+    await fetchWordPacks()
+    await fetchCardPacks(currentWordPackId)
+    setHasInit(true)
+  }
+
+  if (fetchWordPacksLoading || fetchCardPacksLoading || !hasInit) {
+    return (
+      <div className="h-full">
+        <Loading text="加载卡包中..." size="md" />
+      </div>
+    )
+  }
+
+  if (!hasData) {
+    return (
+      <div className="h-full pb-4">
+        <EmptyWordPack showImportButton />
+      </div>
+    )
+  }
 
   return (
     <>
-      {isNil(latestCardPackId) && (
+      {(isNil(latestCardPackId) ||
+        !cardPacks.find((cardPack) => cardPack.id === latestCardPackId)) && (
         <div className="bg-background text-primary/90 p-2 shadow-lg rounded-full text-center font-bold mb-4">
           选择卡包，开始学习吧~
         </div>
       )}
-      <CardPackList />
+      <CardPackList cardPacks={cardPacks} />
       <CardPackConfigModal />
     </>
   )
